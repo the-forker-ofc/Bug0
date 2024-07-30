0e0379eabab8f8d871265f3d2df88c7a23221bd8
the-forker-ofc <404mrfrank@gmail.com>
2024-07-31 00:32:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -23,13 +23,13 @@ global.location = '1' //Ganti Pake Location Panel Lu
 //===========================//
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['254104301695']
-global.ownMain = '254104301695'
-global.NamaOwner = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //gausah diganti 
+global.owner = ['263719647303']
+global.ownMain = '263719647303'
+global.NamaOwner = 'Mr Frank' //gausah diganti 
 global.sessionName = 'spidersession'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
 global.namabot = '𝐒𝐏𝐈𝐃𝐄𝐑-𝐕4' //ganti aj klo mau
-global.author = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //ganti aj klo mau
+global.author = 'Mr Frank' //ganti aj klo mau
 global.packname = '𝚂𝚙𝚒𝚍𝚎𝚛 𝙱𝚘𝚝' //ganti aj klo mau
 global.yt = 'https://chat.whatsapp.com/EPSGKau0IVi7J5lyOJO7Jk' //gausah diganti
 
@@ -46,9 +46,9 @@ global.listr = ` │⬡ 1GB Cpu: 50% - 3.000/bln
  
 //===========================//
 
-global.country = `254`
+global.country = `263`
 global.system = {
-gmail: `samuelcircute@gmail.com`,
+gmail: `darrelmucheri@gmail.com`,
 }
 
 //===========================//
